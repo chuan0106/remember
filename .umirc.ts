b4577b25cb432f2ba1c94d66d3e5f782559b94f5
chuan0106 <xiaochuan199916@sina.com>
2022-04-29 16:34:37 +0800

    更新chainWebpack

--- .umirc.ts
+++ .umirc.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'umi';
-
 export default defineConfig({
     nodeModulesTransform: {
         type: 'none',
@@ -51,5 +50,19 @@ export default defineConfig({
     fastRefresh: {},
     publicPath: './',  // 配置 publicPath ！！！
     // title: '首页',  // 修改title  
-    title: false  // 如果需要自行通过 react-helmet 等方式渲染 title，配 title: false 可禁用内置的 title 渲染机制
+    title: false,  // 如果需要自行通过 react-helmet 等方式渲染 title，配 title: false 可禁用内置的 title 渲染机制
+
+    chainWebpack(config, { webpack }) {
+        console.log([
+            '😀 😁 😂 🤣 😃',
+            '😄 😅 😆 😉 😊',
+            '😫 😴 😌 😛 😜',
+            '👆🏻 😒 😓 😔 👇🏻']);
+        config.merge({
+            plugins: [
+                new webpack.BannerPlugin('版权归属臧小川所有')
+            ],
+        })
+        config.plugins.get('版权归属臧小川所有')
+    },
 });
